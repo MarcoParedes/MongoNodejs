1b2bbcea9fc6335b445e914fbfc8839494a3f627
Marco Paredes <marcop_93_04@hotmail.com>
2017-10-16 18:12:56 -0300

    Express Router

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 5000;
@@ -8,6 +11,9 @@ const port = 5000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
 
